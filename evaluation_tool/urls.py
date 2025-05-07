140ce2e2176c3daa3566ce5123075c4d791bc257
Jorge Benítez López <jorgebenitezlopez@gmail.com>
2025-05-07 19:19:50 +0200

    Try auto deploy

--- evaluation_tool/urls.py
+++ evaluation_tool/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.http import JsonResponse
 
 def api_root(request):
-    return JsonResponse({"message": "API evaluation 0.0.7"})
+    return JsonResponse({"message": "API evaluation 0.0.9"})
 
 urlpatterns = [
     path('', api_root),
